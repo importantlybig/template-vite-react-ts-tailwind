b73505e527dff908efa37a4f133e210ccd10f625
importantlybig <trongdai27122000@gmail.com>
2023-04-06 06:39:16 +0700

    init template

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+function App() {
+	return (
+		<div className='text-center text-3xl'>
+			Setup Vite + React + TS + Tailwind
+		</div>
+	);
+}
+
+export default App;
